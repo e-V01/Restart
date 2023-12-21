cc07f7c1df151512adf432164b3d013554a8cfa4
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-12-21 18:16:32 +0200

    mostlt work around custom animation and ternary operators to adjust the onapeear screen

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -12,6 +12,7 @@ struct OnboardingView: View {
     
     @State private var buttonWidth: Double = UIScreen.main.bounds.width  - 80
     @State private var buttonOffset: CGFloat = 0
+    @State private var isAnimating: Bool = false
     var body: some View {
         ZStack {
             Color("ColorBlue")
@@ -35,12 +36,17 @@ It's not how much we give but how much love we put into giving.
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 10)
                 } // HEADER
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : -40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
                 //MARK: - CEnter
                 ZStack {
                     CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 1), value: isAnimating)
                 } //Center
                 Spacer()
                 //MARK: - Footer
@@ -89,13 +95,15 @@ It's not how much we give but how much love we put into giving.
                                 }
                             }
                             .onEnded { _ in
-                                if buttonOffset > buttonWidth / 2 {
-                                    buttonOffset = buttonWidth - 80
-                                    isOnboardingViewActive = false
-                                } else {
-                                    buttonOffset = 0
+                                withAnimation(Animation.easeOut(duration: 0.4)) {
+                                    if buttonOffset > buttonWidth / 2 {
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
+                                buttonOffset = 0 // button return into same spot after dragging stopped
                                 }
-                            buttonOffset = 0 // button return into same spot after dragging stopped
                             }
                     
                     )// GESTURE
@@ -105,8 +113,14 @@ It's not how much we give but how much love we put into giving.
                 }// FOOTER
                 .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : 40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
             }// VSTACK
         }// ZSTACK
+        .onAppear(perform: {
+            isAnimating = true
+        })
     }
 }
 
